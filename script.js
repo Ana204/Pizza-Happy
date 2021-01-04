7607608f7ce47120e8c2b82766822a6c67430926
ana204 <anagomes.lucia@hotmail.com>
2021-01-04 18:35:57 -0300

    pizza prices

--- script.js
+++ script.js
@@ -116,8 +116,13 @@ function updateCart() {
         elemento('aside').classList.add('show');
         elemento('.cart').innerHTML = '';
 
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart){
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id)
+            subtotal += pizzaItem.price * cart[i].qt;
             let cartItem = elemento('.models .cart--item').cloneNode(true);
             let pizzaSizeName;
 
@@ -139,15 +144,28 @@ function updateCart() {
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
-            cartItem.querySelector('cart--item-qtmenos').addEventListener('click', () => {
-
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                }else{
+                    cart.splice(i, 1);
+                }
+                updateCart();
             });
-            cartItem.querySelector('cart--item-qtmais').addEventListener('click', () => {
-                
-            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
 
             elemento('.cart').append(cartItem);
         }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        elemento('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        elemento('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        elemento('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     } else {
         elemento('aside').classList.remove('show');
     }
